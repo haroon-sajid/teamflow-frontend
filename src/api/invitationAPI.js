959a30d53043b222c6868ef7c2933a7064f34c63
Haroon Sajid <174816863+haroon-sajid@users.noreply.github.com>
2025-10-17 09:37:36 +0500

    update frontend structure and code

--- src/api/invitationAPI.js
+++ src/api/invitationAPI.js
@@ -24,7 +24,8 @@ export const invitationAPI = {
 
 
 // src/api/invitationAPI.js  (or add to existing invitationAPI file)
-const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+// const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+const API_URL = import.meta.env.VITE_API_URL?.trim() || "https://teamflow-backend-1tt9.onrender.com";
 
 function authHeaders() {
   const token = localStorage.getItem("token");
