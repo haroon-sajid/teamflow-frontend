19d9c2d59ec619aeb062e6bd21506964fe6c07ce
Haroon Sajid <174816863+haroon-sajid@users.noreply.github.com>
2025-11-06 15:08:49 +0500

    update frontend  structure and code

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1500, // Increase from default 500 KB to 1.5 MB
+  },
 })
